ec928623476d3947ef74cfbd61402b6cd8e590cc
Alex <bozeak@gmail.com>
2018-07-02 14:26:46 +0300

    EASYOPAC-911 - Changes to Event CT Lists.

---
+++ js/event_node.culture_frontend.js
@@ -0,0 +1,25 @@
+(function($) {
+  'use strict';
+
+  Drupal.behaviors.event_node_culture_frontend = {
+    attach: function () {
+      let lists = [
+          'controlled-similar-list',
+          'controlled-category-list',
+          'controlled-day-list',
+          'controlled-library-list',
+          'controlled-manually-list',
+      ];
+
+      let slick_options = {
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      };
+
+      $.each(lists, function(index, list) {
+        $('.' + list + ' .view-content').slick(slick_options);
+      });
+    }
+  }
+})(jQuery);
