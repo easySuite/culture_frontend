ed66240d1817913d93de9a904e4652fc31ea99ea
Mihaela Stavila <ms@inleadmedia.com>
2018-07-03 15:14:18 +0300

    EASYOPAC-911 - Add breakpoint for slick carousel.

--- js/event_node.culture_frontend.js
+++ js/event_node.culture_frontend.js
@@ -15,6 +15,29 @@
         infinite: false,
         slidesToShow: 3,
         slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 500,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          }
+        ]
       };
 
       $.each(lists, function(index, list) {
